aadf3c5d0d787c1b12d5206739b66eaec19c1530
HamiltonDiaz <diaz3220@hotmail.com>
2023-01-24 21:41:39 -0500

    Prueba cambio git2

--- src/semanaunoesp/Semanaunoesp.java
+++ src/semanaunoesp/Semanaunoesp.java
@@ -22,7 +22,7 @@ public class Semanaunoesp extends javax.swing.JFrame {
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent evt) {
                 exitForm(evt);
-                //prueba cambio
+                
             }
 
         });
